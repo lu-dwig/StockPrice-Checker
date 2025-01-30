7b7b5d17c6f79fb70a3923b866bfff1c36d497c4
lu-dwig <murimiludwig@gmail.com>
2025-01-30 22:18:34 +0300

    api requesting

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,19 @@
 'use strict';
+const StockModel = require("../models").StockModel;
+const fetch = require ("node-fetch");
+
 
 module.exports = function (app) {
 
   // https://stock-price-checker-proxy.freecodecamp.rocks/
-  
+
   app.route('/api/stock-prices')
     .get(function (req, res){
+      const { stocks, like } = req.query;
       
+      // if (!stocks || stocks.length === 0) {
+      //   return res.status(400).json({ error: 'No stocks provided' });
+      // }
     });
     
 };

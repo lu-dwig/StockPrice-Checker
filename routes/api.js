d1ab0e2a9628974a09ff895ab65fed32502660bf
lu-dwig <murimiludwig@gmail.com>
2025-01-30 22:34:55 +0300

    modification  of async function for api routing

--- routes/api.js
+++ routes/api.js
@@ -14,10 +14,13 @@ module.exports = function (app) {
 
   // https://stock-price-checker-proxy.freecodecamp.rocks/
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      const { stocks, like } = req.query;
-
+  app.route('/api/stock-prices').get(async function (req, res){
+      const { stock, like } = req.query;
+      const { symbol, latestPrice } = await getStock(stock);
+      if (!symbol){
+        res.jsons({ stockData: { likes: like ? 1 :0 } });
+        return;
+      }
       // if (!stocks || stocks.length === 0) {
       //   return res.status(400).json({ error: 'No stocks provided' });
       // }

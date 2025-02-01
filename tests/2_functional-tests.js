aa03f199b3f0506314fb6f34b1f5382625fef1a7
lu-dwig <murimiludwig@gmail.com>
2025-02-01 21:29:35 +0300

    creation of the fist test fo  viewing one stock

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,19 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    suite("5 functional get request tests", function () {
+        test("Viewing one stock: GET request to /api/stock-prices/", function (done) {
+          chai
+            .request(server)
+            .get("/api/stock-prices/")
+            .set("content-type", "application/json")
+            .query({ stock: "TSLA" })
+            .end(function (err, res) {
+              assert.equal(res.status, 200);
+              assert.equal(res.body.stockData.stock, "TSLA");
+              assert.exists(res.body.stockData.price, "TSLA has a price");
+              done();
+            });
+        });
+    });
 });
